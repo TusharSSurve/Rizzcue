5c16d3032a8807e1a729baa78719975f9765bfbe
TusharSSurve <ttusharsurve567@gmail.com>
2025-06-22 22:32:55 +0530

    feat: spring boot

---
+++ spring-boot/src/main/java/com/example/rizzcue/RizzcueApplication.java
@@ -0,0 +1,13 @@
+package com.example.rizzcue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RizzcueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RizzcueApplication.class, args);
+	}
+
+}
---
+++ spring-boot/src/test/java/com/example/rizzcue/RizzcueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rizzcue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RizzcueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
