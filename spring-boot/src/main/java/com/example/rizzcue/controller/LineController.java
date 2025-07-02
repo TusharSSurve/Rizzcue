7a3b63d8eb2b2c6274dee6d213a2ef937b626e44
TusharSSurve <ttusharsurve567@gmail.com>
2025-07-02 16:39:23 +0530

    refactor(spring-boot): Files

--- spring-boot/src/main/java/com/example/rizzcue/controller/LineController.java
+++ spring-boot/src/main/java/com/example/rizzcue/controller/LineController.java
@@ -1,4 +1,15 @@
 package com.example.rizzcue.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class LineController {
+
+    @GetMapping("lines")
+    public String getLines() {
+        return "This is a placeholder for the lines endpoint.";
+    }
 }
--- spring-boot/src/main/java/com/example/rizzcue/model/Line.java
+++ spring-boot/src/main/java/com/example/rizzcue/model/Line.java
@@ -1,4 +1,20 @@
 package com.example.rizzcue.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Line {
+    @Id
+    private String id;
+    private String line1;
+    private String line2;
+    private boolean is_nsfw;
+    private int likes;
 }
